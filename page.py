4f9b224f9ff8b1ba27b483c593a239d42cbbd42e
Corentin <corentin.beuf@ynov.com>
2020-04-17 12:28:44 +0200

    Ajout de commentaire

--- page.py
+++ page.py
@@ -14,7 +14,7 @@ champ_label = Label(fenetre, text="Projet B1 Brick Shooter !")
 champ_label.pack()
 
 
-#Apparition de l'image en fond 
+#Apparition de l'image en fond
 canvas=Canvas(fenetre)
 canvas.pack()
 
@@ -22,9 +22,7 @@ photo=PhotoImage(file="noir.png")
 canvas.create_image(370, 250, image=photo)
 
 
-
-
-
+#Fonction pour les boutons
 bouton_quitter = Button(fenetre, text="Quitter", command=fenetre.quit)
 bouton_quitter.pack(side="bottom")
 bouton_instruction = Button(fenetre, text="Instruction")
@@ -33,7 +31,6 @@ bouton_personnaliser = Button(fenetre, text="Personnaliser")
 bouton_personnaliser.pack(side="bottom")
 bouton_jouer = Button(fenetre, text="Jouer")
 bouton_jouer.pack(side="bottom")
-
 bouton_jouer.configure(width=20, height=2)
 bouton_personnaliser.configure(width=20, height=2)
 bouton_instruction.configure(width=20, height=2)
