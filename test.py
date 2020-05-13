543926095966760d5ca35e232ed7ddcacb76b152
Corentin <corentin.beuf@ynov.com>
2020-05-13 16:09:46 +0200

    Suppression de dossier inutile

--- test.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-
-def chargerImage(): 
-    im=Image.open("noir .png") 
-    photo = ImageTk.PhotoImage(im) 
-    dicimg['fond'] = photo
-    item = cadre.create_image(320,240,image =photo) 
-    print("charger image")
-    
